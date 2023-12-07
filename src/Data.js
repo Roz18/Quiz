9c1fe53e1562f64a5dd68afdf70a43f129569703
Roz18 <114506732+Roz18@users.noreply.github.com>
2023-12-07 21:13:28 +0100

    Q&A
    added the questions and answers

---
+++ src/Data.js
@@ -0,0 +1,68 @@
+const data = [
+  {
+    question: "What is a Cowplant?",
+    incorrectAnswers: [
+      "A plant that has spotted berries that mimics cow patterns.",
+      "A plant that has milky fruit and makes mooing noises.",
+      "It is a decorative pot plant that mimics a cow.",
+    ],
+    correctAnswer: "Mutant plant that has a cow head and plant body.",
+  },
+
+  {
+    question: "How do you say hi in Simlish?",
+    incorrectAnswers: ["Nooboo", "Woofum", "Lalo"],
+    correctAnswer: "Sul sul",
+  },
+  {
+    question: "A male sim being abducted by aliens can result in?",
+    incorrectAnswers: [
+      "Becoming an alien.",
+      "Loses all memories.",
+      "Gain rocket science skill.",
+    ],
+    correctAnswer: "Become pregnant.",
+  },
+  {
+    question: "What is a Plumbob?",
+    incorrectAnswers: [
+      "A plumber robot.",
+      "A weird plum tree.",
+      "A tool to determine a true vertical.",
+    ],
+    correctAnswer: "The gem above the character's head.",
+  },
+  {
+    question: "What is the curse of the Night Wraith?",
+    incorrectAnswers: [
+      "A scary decorative item that looks like a ghost.",
+      "A ghostly figure will water your flowers and kill them in the process.",
+      "A scary woman would appear in your house and cry all night long.",
+    ],
+    correctAnswer:
+      "A ghostly figure will stalk your Sim, randomly scare them, and you will not be able to sleep.",
+  },
+  {
+    question: "How many hidden areas are there in the Sims 4?",
+    incorrectAnswers: ["2", "7", "5"],
+    correctAnswer: "4",
+  },
+  {
+    question:
+      "What cheat do you use to remove building constraints in build mode?",
+    incorrectAnswers: ["testingCheats true", "motherlode", "cas.fulleditmode"],
+    correctAnswer: "bb.moveobjects",
+  },
+  {
+    question:
+      "What will happen if your sim gifts a floral arrangement scented with a death flower to another sim?",
+    incorrectAnswers: [
+      "It will summon the Grimm",
+      "Nothing, the sim will just have a stinky floral arrangement.",
+      "The sim will turn into the Grimm",
+    ],
+    correctAnswer: "The sim  will age up a whole life stage.",
+  },
+];
+
+export default data;
